895244d5ed0b701e9447e08725047e2281ccafae
Rios <1556544189157002@mil>
2023-06-08 15:48:59 -0400

    Updated current files

--- ipython_script_test.py
+++ ipython_script_test.py
@@ -1,8 +1,12 @@
 def f(x, y, z):
     return (x + y) / z
 
-a = 5
-b = 6
-c = 7.5
+if __name__ == '__main__':
+    a = 5
+    b = 6
+    c = 7.5
 
-result = f(a, b, c)
+    result = f(a, b, c)
+
+else:
+    pass
